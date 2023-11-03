a4e56c963f0b81a61d0ba3ab6c42f813144132db
Phil <philllllau@gmail.com>
2023-11-03 10:34:08 +0800

    Tidy FeeManager.

--- contracts/FeeManager.sol
+++ contracts/FeeManager.sol
@@ -9,7 +9,7 @@ import "./lib/Helper.sol";
 contract FeeManager is Ownable2Step, IFeeManager {
     uint256 constant FEE_DENOMINATOR = 10000;
 
-    struct IntegratorFeeInfo {
+    struct FeeInfo {
         address openliqReceiver;
         uint256 fixedNative;
         uint256 tokenFeeRate;
@@ -26,10 +26,10 @@ contract FeeManager is Ownable2Step, IFeeManager {
     FeeStruct public feeStruct;
 
     // Integrator -> IntegratorFeeInfo
-    mapping(address => IntegratorFeeInfo) public integratorToFeeInfo;
+    mapping(address => FeeInfo) public feeInfoList;
 
-    event SetIntegratorFees(address integrator, IntegratorFeeInfo fee);
-    event InitialFeeStruct(FeeStruct _feeStruct);
+    event SetIntegratorFees(address integrator, FeeInfo fee);
+    event InitialFeeStruct(FeeStruct feeStruct);
 
     constructor(address _owner) payable {
         require(_owner != Helper.ZERO_ADDRESS, ErrorMessage.ZERO_ADDR);
@@ -44,121 +44,121 @@ contract FeeManager is Ownable2Step, IFeeManager {
         emit InitialFeeStruct(_feeStruct);
     }
 
-    function setIntegratorFees(address integrator, IntegratorFeeInfo calldata fees) external onlyOwner {
+    function setIntegratorFee(address _integrator, FeeInfo calldata _feeInfo) external onlyOwner {
         require(
-            integrator != Helper.ZERO_ADDRESS && fees.openliqReceiver != Helper.ZERO_ADDRESS,
+            _integrator != Helper.ZERO_ADDRESS && _feeInfo.openliqReceiver != Helper.ZERO_ADDRESS,
             ErrorMessage.ZERO_ADDR
         );
-        require(fees.tokenFeeRate < FEE_DENOMINATOR, "FeeManager: invalid integrator tokenFeeRate");
-        require(fees.share <= FEE_DENOMINATOR, "FeeManager: invalid integrator share");
-        integratorToFeeInfo[integrator] = fees;
-        emit SetIntegratorFees(integrator, fees);
+        require(_feeInfo.tokenFeeRate < FEE_DENOMINATOR, "FeeManager: invalid integrator tokenFeeRate");
+        require(_feeInfo.share <= FEE_DENOMINATOR, "FeeManager: invalid integrator share");
+        feeInfoList[_integrator] = _feeInfo;
+        emit SetIntegratorFees(_integrator, _feeInfo);
     }
 
     function getFee(
-        address integrator,
-        address inputToken,
-        uint256 inputAmount,
-        uint256 feeP
-    ) external view returns (FeeDetail memory returnFee) {
-        require(feeP < FEE_DENOMINATOR);
+        address _integrator,
+        address _inputToken,
+        uint256 _inputAmount,
+        uint256 _feeRate
+    ) external view returns (FeeDetail memory feeDetail) {
+        require(_feeRate < FEE_DENOMINATOR);
         FeeStruct memory f = feeStruct;
-        IntegratorFeeInfo memory info = integratorToFeeInfo[integrator];
-        returnFee.feeToken = inputToken;
-        if (info.openliqReceiver == address(0)) {
-            if (integrator != Helper.ZERO_ADDRESS && feeP > 0) {
-                uint256 fee = (inputAmount * feeP) / FEE_DENOMINATOR;
-                returnFee.openLiqToken = (fee * f.share) / FEE_DENOMINATOR;
-                returnFee.integratorToken = fee - returnFee.openLiqToken;
+        FeeInfo memory info = feeInfoList[_integrator];
+        feeDetail.feeToken = _inputToken;
+        if (info.openliqReceiver == Helper.ZERO_ADDRESS) {
+            if (_integrator != Helper.ZERO_ADDRESS && _feeRate > 0) {
+                uint256 fee = (_inputAmount * _feeRate) / FEE_DENOMINATOR;
+                feeDetail.openLiqToken = (fee * f.share) / FEE_DENOMINATOR;
+                feeDetail.integratorToken = fee - feeDetail.openLiqToken;
             }
-            returnFee.openliqNative = f.fixedNative;
+            feeDetail.openliqNative = f.fixedNative;
             if (f.tokenFeeRate > 0) {
-                uint256 tokenFee = (inputAmount * f.tokenFeeRate) / FEE_DENOMINATOR;
+                uint256 tokenFee = (_inputAmount * f.tokenFeeRate) / FEE_DENOMINATOR;
                 if (f.fixedNative == 0) {
-                    returnFee.openLiqToken += tokenFee;
+                    feeDetail.openLiqToken += tokenFee;
                 } else {
-                    if (tokenFee > returnFee.integratorToken) {
-                        returnFee.openLiqToken = tokenFee;
+                    if (tokenFee > feeDetail.integratorToken) {
+                        feeDetail.openLiqToken = tokenFee;
                     }
                 }
             }
-            returnFee.openliqReceiver = f.receiver;
+            feeDetail.openliqReceiver = f.receiver;
         } else {
-            if (integrator != Helper.ZERO_ADDRESS && feeP > 0) {
-                uint256 fee = (inputAmount * feeP) / FEE_DENOMINATOR;
-                returnFee.openLiqToken = (fee * info.share) / FEE_DENOMINATOR;
-                returnFee.integratorToken = fee - returnFee.openLiqToken;
+            if (_integrator != Helper.ZERO_ADDRESS && _feeRate > 0) {
+                uint256 fee = (_inputAmount * _feeRate) / FEE_DENOMINATOR;
+                feeDetail.openLiqToken = (fee * info.share) / FEE_DENOMINATOR;
+                feeDetail.integratorToken = fee - feeDetail.openLiqToken;
             }
-            returnFee.openliqNative = info.fixedNative;
+            feeDetail.openliqNative = info.fixedNative;
             if (info.tokenFeeRate > 0) {
-                uint256 tokenFee = (inputAmount * info.tokenFeeRate) / FEE_DENOMINATOR;
-                if (tokenFee > returnFee.openLiqToken) {
-                    returnFee.openLiqToken = tokenFee;
+                uint256 tokenFee = (_inputAmount * info.tokenFeeRate) / FEE_DENOMINATOR;
+                if (tokenFee > feeDetail.openLiqToken) {
+                    feeDetail.openLiqToken = tokenFee;
                 }
             }
-            returnFee.openliqReceiver = info.openliqReceiver;
+            feeDetail.openliqReceiver = info.openliqReceiver;
         }
     }
 
     function getAmountBeforeFee(
-        address integrator,
-        address inputToken,
-        uint256 inputAmount,
-        uint256 feeP
+        address _integrator,
+        address _inputToken,
+        uint256 _inputAmount,
+        uint256 _feeRate
     ) external view returns (address feeToken, uint256 beforeAmount) {
-        require(feeP < FEE_DENOMINATOR);
+        require(_feeRate < FEE_DENOMINATOR);
         FeeStruct memory f = feeStruct;
-        IntegratorFeeInfo memory info = integratorToFeeInfo[integrator];
-        feeToken = inputToken;
+        FeeInfo memory info = feeInfoList[_integrator];
+        feeToken = _inputToken;
         if (info.openliqReceiver == address(0)) {
-            if (Helper._isNative(inputToken)) {
-                inputAmount += f.fixedNative;
+            if (Helper._isNative(_inputToken)) {
+                _inputAmount += f.fixedNative;
             }
-            if (integrator != Helper.ZERO_ADDRESS && feeP > 0) {
+            if (_integrator != Helper.ZERO_ADDRESS && _feeRate > 0) {
                 if (f.fixedNative == 0) {
-                    uint256 p = feeP + f.tokenFeeRate;
-                    beforeAmount = (inputAmount * FEE_DENOMINATOR) / (FEE_DENOMINATOR - p) + 1;
+                    uint256 p = _feeRate + f.tokenFeeRate;
+                    beforeAmount = (_inputAmount * FEE_DENOMINATOR) / (FEE_DENOMINATOR - p) + 1;
                 } else {
-                    if (f.tokenFeeRate * FEE_DENOMINATOR > feeP * f.share) {
+                    if (f.tokenFeeRate * FEE_DENOMINATOR > _feeRate * f.share) {
                         uint256 p = FEE_DENOMINATOR *
                             FEE_DENOMINATOR -
                             f.tokenFeeRate *
                             FEE_DENOMINATOR -
                             (FEE_DENOMINATOR - f.share) *
-                            feeP;
-                        beforeAmount = (inputAmount * FEE_DENOMINATOR * FEE_DENOMINATOR) / p + 1;
+                            _feeRate;
+                        beforeAmount = (_inputAmount * FEE_DENOMINATOR * FEE_DENOMINATOR) / p + 1;
                     } else {
-                        beforeAmount = (inputAmount * FEE_DENOMINATOR) / (FEE_DENOMINATOR - feeP) + 1;
+                        beforeAmount = (_inputAmount * FEE_DENOMINATOR) / (FEE_DENOMINATOR - _feeRate) + 1;
                     }
                 }
             } else {
                 if (f.tokenFeeRate > 0) {
-                    beforeAmount = (inputAmount * FEE_DENOMINATOR) / (FEE_DENOMINATOR - f.tokenFeeRate) + 1;
+                    beforeAmount = (_inputAmount * FEE_DENOMINATOR) / (FEE_DENOMINATOR - f.tokenFeeRate) + 1;
                 } else {
-                    beforeAmount = inputAmount;
+                    beforeAmount = _inputAmount;
                 }
             }
         } else {
-            if (Helper._isNative(inputToken)) {
-                inputAmount += f.fixedNative;
+            if (Helper._isNative(_inputToken)) {
+                _inputAmount += f.fixedNative;
             }
-            if (integrator != Helper.ZERO_ADDRESS && feeP > 0) {
-                if (info.tokenFeeRate * FEE_DENOMINATOR > feeP * info.share) {
+            if (_integrator != Helper.ZERO_ADDRESS && _feeRate > 0) {
+                if (info.tokenFeeRate * FEE_DENOMINATOR > _feeRate * info.share) {
                     uint256 p = FEE_DENOMINATOR *
                         FEE_DENOMINATOR -
                         info.tokenFeeRate *
                         FEE_DENOMINATOR -
                         (FEE_DENOMINATOR - info.share) *
-                        feeP;
-                    beforeAmount += (inputAmount * FEE_DENOMINATOR * FEE_DENOMINATOR) / p + 1;
+                        _feeRate;
+                    beforeAmount += (_inputAmount * FEE_DENOMINATOR * FEE_DENOMINATOR) / p + 1;
                 } else {
-                    beforeAmount = (inputAmount * FEE_DENOMINATOR) / (FEE_DENOMINATOR - feeP) + 1;
+                    beforeAmount = (_inputAmount * FEE_DENOMINATOR) / (FEE_DENOMINATOR - _feeRate) + 1;
                 }
             } else {
                 if (info.tokenFeeRate > 0) {
-                    beforeAmount = (inputAmount * FEE_DENOMINATOR) / (FEE_DENOMINATOR - info.tokenFeeRate) + 1;
+                    beforeAmount = (_inputAmount * FEE_DENOMINATOR) / (FEE_DENOMINATOR - info.tokenFeeRate) + 1;
                 } else {
-                    beforeAmount = inputAmount;
+                    beforeAmount = _inputAmount;
                 }
             }
         }
